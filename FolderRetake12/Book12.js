058d29cb08a7eeeae634086817092fc7130ba716
Veerapat <93991274+AunLn9@users.noreply.github.com>
2024-03-14 10:36:25 +0700

    Update Book12.js

--- FolderRetake12/Book12.js
+++ FolderRetake12/Book12.js
@@ -17,33 +17,48 @@ class Book {
     this.ebook = ebook
     this.authors = []
   }
-  //   addAuthor(fn, ln): creates an author object by assigning the firstnameproperty with the provided parameter named fnand the lastnameproperty with the provided parameter named ln. Then adding it to the end of authors array. You must return the new length of the authorsarray.
-  addAuthor(fn, ln) {}
+  //   addAuthor(fn, ln): creates an author object by assigning the firstname property with the provided parameter named nand the lastname property with the provided parameter named ln. Then adding it to the end of authors array. You must return the new length of the authors array.
+  addAuthor(fn, ln) {
+    return this.authors.push({ firstname: fn, lastname: ln })
+  }
   // getFirstAuthor() : returns the first author object of book, if no author then returns undefined.
-  getFirstAuthor() {}
+  getFirstAuthor() {
+    return this.authors[0]
+  }
+  getLastAuthor() {
+    return this.authors[this.authors.length - 1]
+  }
   //   getCoAuthors(): returns array of all authors except the first author, if no co-author, returns an empty array ([]).
-  getCoAuthors() {}
-  //   findAuthor(firstname, lastname): returns the first author object that matches both firstnameand lastnameparameters (case insensitive). If no author matches, undefined is returned.
-  findAuthor(firstname, lastname) {}
-  // hasEbook() : returns the ebookproperty’ s value.
-  hasEbook() {}
+  getCoAuthors() {
+    // return this.authors.slice(1, this.authors.length)
+    const [, ...coauthors] = this.authors
+    return coauthors
+  }
+  //   findAuthor(firstname, lastname): returns the first author object that matches both firstname and lastname parameters (case insensitive). If no author matches, undefined is returned.
+  findAuthor(firstname, lastname) {
+    return this.authors.find(
+      (author) =>
+        author.firstname.toLowerCase() === firstname.toLowerCase() &&
+        author.lastname.toLowerCase() === lastname.toLowerCase()
+    )
+  }
+  // hasEbook() : returns the ebook property’ s value.
+  hasEbook() {
+    return this.ebook
+  }
 }
 
 const bk1 = new Book()
+console.log(bk1.addAuthor('Somsak', 'Jaidee')) //1
+console.log(bk1.addAuthor('Somsri', 'Jaidee')) //2
+console.log(bk1.addAuthor('Somchai', 'Jaidee')) //3
 console.log(bk1)
 const bk2 = new Book('1-234-567-8', 'Introduction to JavaScript', true)
+console.log(bk2.addAuthor('John', 'May'))
 console.log(bk2)
-//object literals
-const bk3 = {
-  id: 1,
-  bookTitle: 'JS Beginner',
-  price: 500,
-  author: { firstname: 'John', lastname: 'Dee' }
-}
-console.log(bk3)
-const bk4 = {
-  isbn: '333-222-444',
-  name: 'Advanced JS',
-  author: 'Somchai Jaidee'
-}
-console.log(bk4)
+
+console.log(bk1.getFirstAuthor())
+console.log(bk1.getLastAuthor())
+console.log(bk1.getCoAuthors())
+console.log(bk1.findAuthor('Somchai', 'JAIDEE'))
+console.log(bk1.hasEbook())
